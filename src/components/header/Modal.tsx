f14cdbbe1eabd7a2c7488755b43d8ab445d1ce6f
misanc21 <misanc21@gmail.com>
2025-01-25 15:01:46 -0600

    adding modal

--- src/components/header/Modal.tsx
+++ src/components/header/Modal.tsx
@@ -4,19 +4,23 @@ import { motion } from "motion/react"
 import { useGlobalState } from "@/app/context/Context";
 
 export const Modal = () => {
-  const { showModal } = useGlobalState();
+  const { showModal, setShowModal } = useGlobalState();
   return (
     <AnimatePresence>
       {
         showModal &&
-        <motion.div
-          key="Modal"
-          initial="hidden"
-          animate="visible"
-          exit="hidden"
-        >
-          hello
-        </motion.div>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={() => setShowModal(!showModal)}>
+          <motion.div
+            key="Modal"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            className="relative bg-black rounded-lg shadow-lg p-6 w-full max-w-lg"
+            onClick={e => e.stopPropagation()}
+          >
+            <span className="text-gray-200">On building</span>
+          </motion.div>
+        </div>
       }
     </AnimatePresence>
   )
