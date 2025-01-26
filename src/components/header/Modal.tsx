87b6bc31c6345bfa1f4c8c49857be2a33dca1a19
misanc21 <misanc21@gmail.com>
2025-01-26 16:15:30 -0600

    addin transitions to modal

--- src/components/header/Modal.tsx
+++ src/components/header/Modal.tsx
@@ -9,18 +9,31 @@ export const Modal = () => {
     <AnimatePresence>
       {
         showModal &&
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={() => setShowModal(!showModal)}>
+        <motion.div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={() => setShowModal(!showModal)}
+          key="backdrop"
+          initial={{ opacity: 0, scale: 1, filter: "blur(10px)" }}
+          animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
+          exit={{ opacity: 0, scale: 1, filter: "blur(10px)" }}
+          transition={{ duration: 0.4 }}
+          style={{ perspective: 1000 }}
+        >
           <motion.div
             key="Modal"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0 }}
-            className="relative bg-black rounded-lg shadow-lg p-6 w-full max-w-lg"
+            initial={{ opacity: 0, scale: 1, rotateX: -180 }}
+            animate={{ opacity: 1, scale: 1, rotateX: 0 }}
+            exit={{ opacity: 0, scale: 1, rotateX: 180 }}
+            className="relative bg-black rounded-lg shadow-lg p-6 w-full max-w-lg origin-top"
             onClick={e => e.stopPropagation()}
+            transition={{ duration: 0.3, ease: [0.17, 0.67, 0.83, 1] }}
+            style={{
+              transformStyle: "preserve-3d",
+              perspective: 1000,
+            }}
           >
             <span className="text-gray-200">On building</span>
           </motion.div>
-        </div>
+        </motion.div>
       }
     </AnimatePresence>
   )
