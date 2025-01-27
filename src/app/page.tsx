ddb7ac5686abb6b3424e06819cf398f79eb06e0a
misanc21 <misanc21@gmail.com>
2025-01-27 14:19:56 -0600

    fixin modal button and adding style to progress bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,21 @@ export default function Home() {
           right: 0,
           height: 5,
           originX: 0,
-          backgroundColor: '#6dd14f',
           zIndex: 100,
-          borderRadius: '0 10px 10px 0',
+        }}
+        animate={{
+          scaleY: [1, 1.5, 1],
+          background: [
+            "linear-gradient(to left, rgba(43, 185, 0, 0.3)0%,rgba(43, 185, 0, 0.5) 30%,rgba(43, 185, 0, 1)50%",
+            "linear-gradient(to left, rgba(0, 100, 207, 0.3)0%,rgba(0, 100, 207, 0.5)30%, rgba(0, 100, 207,1)50%",
+            "linear-gradient(to left, rgba(240, 112, 0, 0.3)0%,rgba(240, 112, 0, 0.5)30%, rgba(240, 112, 0,1)50%",
+          ],
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "linear",
+          repeatType: "reverse"
         }}
       />
       <div className="relative z-50 overflow-y-auto">
--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useGlobalState } from "@/app/context/Context";
 import { motion } from "motion/react";
-import Link from "next/link";
 
 export default function ContactButton() {
   const { showModal, setShowModal } = useGlobalState();
@@ -12,9 +11,13 @@ export default function ContactButton() {
         whileTap={{ scale: 0.90 }}
         transition={{ type: "spring", stiffness: 300, damping: 10 }}
       >
-        <Link href={"/"} onClick={() => setShowModal(!showModal)} className="bg-gray-700 text-white font-bold py-2 px-4 rounded-lg shadow-md hover:bg-gray-900 transition duration-300">
+        <button
+          onClick={() => setShowModal(!showModal)}
+          className="bg-gray-700 text-white font-bold py-2 px-4 rounded-lg shadow-md hover:bg-gray-900 transition duration-300 hover:cursor-pointer"
+          type="button"
+        >
           Contact me
-        </Link>
+        </button>
       </motion.div>
     </>
   );
